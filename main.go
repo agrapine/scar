0b0c0b491bec82a717e70c35ec1c44577bbce3b8
Alexandru Agrapine <alexandru@agrapine.com>
2019-03-18 21:37:29 +0000

    i think i got some links in

--- main.go
+++ main.go
@@ -2,12 +2,79 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"net/http"
+	"strings"
+
+	"golang.org/x/net/html"
 )
 
+func scar(url string, in chan string, out chan bool) {
+	res, e := http.Get(url)
+	defer func() {
+		out <- true
+	}()
+	if e != nil {
+		return
+	}
+	body := res.Body
+	defer body.Close() //i wish i had this in c#... using will do
+
+	tokens := html.NewTokenizer(body)
+	for {
+		tokenType := tokens.Next()
+
+		switch {
+		case tokenType == html.ErrorToken:
+			return
+		case tokenType == html.StartTagToken:
+			token := tokens.Token()
+			if token.Data != "a" {
+				continue
+			}
+			href, ok := getHref(token)
+			if !ok {
+				continue
+			}
+			if strings.HasPrefix(href, "http") {
+				in <- href
+			}
+		}
+	}
+}
+
+func getHref(token html.Token) (href string, ok bool) {
+	for _, a := range token.Attr {
+		if a.Key == "href" {
+			href = a.Val
+			ok = true
+		}
+	}
+	return
+}
+
 func main() {
-	resp, _ := http.Get("https://www.cuvva.com/")
-	bytes, _ := ioutil.ReadAll(resp.Body)
-	fmt.Println("HTML:\n\n", string(bytes))
+	links := make(map[string]bool)
+	seed := make([]string, 0)
+	seed = append(seed, "https://cuvva.com/")
+
+	in := make(chan string)
+	out := make(chan bool)
+	for _, url := range seed {
+		go scar(url, in, out)
+	}
+
+	for c := 0; c < len(seed); {
+		select { //this is pretty cool
+		case url := <-in:
+			links[url] = true
+		case <-out:
+			c++
+		}
+	}
+
+	for link := range links {
+		fmt.Println(" - " + link)
+	}
+
+	close(in)
 }
